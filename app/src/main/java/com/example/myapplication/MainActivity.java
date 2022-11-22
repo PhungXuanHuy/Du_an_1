fe310f69df2be4196804c550c84240014f62b8bc
hakim0607 <kimha0607.contact@gmail.com>
2022-11-22 19:52:56 +0700

    Chỉnh sửa nhiều cái

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.myapplication.Fragment.HomeFragment;
+import com.example.myapplication.Fragment.ToiFragment;
+import com.example.myapplication.Fragment.XepHangFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    HomeFragment homeFragment;
+    XepHangFragment xepHangFragment;
+    ToiFragment toiFragment;
+    BottomNavigationView bottom_nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,homeFragment).commit();
+        bottom_nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.itHome:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,homeFragment).commit();
+                        return true;
+                    case R.id.itXepHang:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,xepHangFragment).commit();
+                        return true;
+                    case R.id.itToi:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,toiFragment).commit();
+                        return true;
+                }
+                return true;
+            }
+        });
     }
+
+    public void AnhXa() {
+        bottom_nav = findViewById(R.id.bottom_nav);
+        homeFragment = new HomeFragment();
+        xepHangFragment = new XepHangFragment();
+        toiFragment = new ToiFragment();
+    }
+
 }
--- app/src/main/java/com/example/myapplication/Manhinhdangnhap.java
+++ app/src/main/java/com/example/myapplication/Manhinhdangnhap.java
@@ -2,13 +2,44 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Manhinhdangnhap extends AppCompatActivity {
-
+    EditText edTenDangNhap,edMatKhau;
+    Button btnDangNhap;
+    TextView tvDangKy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhdangnhap);
+        AnhXa();
+        tvDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Manhinhdangnhap.this,Manhinhdangky.class);
+                startActivity(intent);
+            }
+        });
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Manhinhdangnhap.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+    private void AnhXa() {
+        edTenDangNhap = findViewById(R.id.edTenDangNhap);
+        edMatKhau = findViewById(R.id.edMatKhau);
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+        tvDangKy = findViewById(R.id.tvDangKy);
+    }
+
 }
