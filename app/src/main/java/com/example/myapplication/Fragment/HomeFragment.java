8c394452347ae4b04cab5a2caf92baf530cc2d93
TRAN QUYNH <quynhtdph21779@fpt.edu.vn>
2022-12-05 17:30:20 +0700

    chỉnh sửa lại phần thêm ngữ pháp dialog

--- app/src/main/java/com/example/myapplication/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/HomeFragment.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.BatDauNguPhapActivity;
